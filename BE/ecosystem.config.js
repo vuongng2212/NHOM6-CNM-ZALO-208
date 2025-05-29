ef2ade182bdd1fc408ed2fc8e416ebb7e0217be7
Vuong Nguyen <vuong@example.com>
2025-05-30 01:45:23 +0700

    config something

--- BE/ecosystem.config.js
+++ BE/ecosystem.config.js
@@ -1,9 +1,21 @@
 module.exports = {
-    apps: [{
-      name: "backend",
-      script: "./server/index.js",
+  apps: [
+    {
+      name: 'chat-backend',
+      script: 'server.js', // hoặc app.js, index.js tùy file chính của bạn
+      cwd: '/home/ubuntu/chat-app/backend',
       env: {
-        NODE_ENV: "production",
-      }
-    }]
-  }
+        NODE_ENV: 'production',
+        PORT: 3000
+      },
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      error_file: './logs/err.log',
+      out_file: './logs/out.log',
+      log_file: './logs/combined.log',
+      time: true
+    }
+  ]
+};
